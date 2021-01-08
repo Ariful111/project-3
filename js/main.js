923167f0035c4b6e61c6ae780f117b4a980f1b90
Ariful111 <Ariful111@users.noreply.github.com>
2021-01-08 15:53:16 +0600

    project-3

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".z").click(function(){
+		$("br").css({"color":"red","background":"orange"});
+	});
+	$(".y").dblclick(function(){
+		$("hr").css("color","black");
+	});
+});
